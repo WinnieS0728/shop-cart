f3d420df1c5b209b46004b60e036f3a13c1f432f
WinnieS0728 <116339731+WinnieS0728@users.noreply.github.com>
2024-02-09 15:11:01 +0800

    Update env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -4,5 +4,7 @@ declare namespace NodeJS {
         NEXTAUTH_GOOGLE_ID: string
         NEXTAUTH_GOOGLE_SECRET: string
         NEXTAUTH_SECRET: string
+        EDGE_STORE_ACCESS_KEY: string
+        EDGE_STORE_SECRET_KEY: string
     }
 }
